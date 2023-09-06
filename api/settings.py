ff69246fe966849e9fcba7c93b691ba228f48672
RISHIRGK <kadamrishi05@gmail.com>
2023-09-06 11:23:10 +0530

    commit

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-rcn91k0tdadg$ttw@n%ukf&qb_ccbjducs)1h-xk@av(bdv3+x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
